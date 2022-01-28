62bec48c394f3f6580ef38d3bf77af8952e5c5b9
m.m <m3farahani1382@gmail.com>
2022-01-28 15:15:32 +0330

    drawer layout completed

--- app/src/main/java/com/example/navigationproject/MainActivity.kt
+++ app/src/main/java/com/example/navigationproject/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.navigationproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.navigationproject.databinding.ActivityMainBinding
@@ -24,6 +27,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
+        if (binding.drawer.isOpen) {
+            binding.drawer.closeDrawer(GravityCompat.START)
+            return true // successful
+        }
         return NavigationUI.navigateUp(controller, binding.drawer)
     }
 
@@ -36,4 +43,21 @@ class MainActivity : AppCompatActivity() {
         return NavigationUI.onNavDestinationSelected(item, controller) ||
                 super.onOptionsItemSelected(item)
     }
+
+    override fun onBackPressed() {
+        binding.run {
+            if (drawer.isOpen) {
+                drawer.closeDrawer(GravityCompat.START)
+                return
+            }
+        }
+        super.onBackPressed()
+    }
+
+    fun call(item: MenuItem) {
+        startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("tel:09123456789")
+        })
+    }
 }
+
